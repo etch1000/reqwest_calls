dd3dc82a62f23d94a66d26ac9c3f85267626807a
etch1000 <hmoroliya@ariqt.com>
2022-10-31 11:14:26 +0530

    Added: Making a POST Request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
 use reqwest::header::CONTENT_TYPE;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct GETAPIResponse {
@@ -18,9 +18,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
 
     // Making a GET Request
-    let response_200 = client.get("https://httpbin.org/ip").header(CONTENT_TYPE, "application/json").send().await?.json::<GETAPIResponse>().await?;
+    let response_200 = client
+        .get("https://httpbin.org/ip")
+        .header(CONTENT_TYPE, "application/json")
+        .send()
+        .await?
+        .json::<GETAPIResponse>()
+        .await?;
 
     println!("{:#?}", response_200);
+
+    // Create a Map of string key-value pairs
+    // to represent the body payload
+    let mut map = HashMap::new();
+
+    map.insert("lang", "rust");
+    map.insert("body", "json");
+
+    // Making a POST Request
+    let response_json = client.post("https://httpbin.org/anything").json(&map).send().await?.json::<JSONResponse>().await?;
+
+    println!("{:#?}", response_json);
 
     Ok(())
 }
